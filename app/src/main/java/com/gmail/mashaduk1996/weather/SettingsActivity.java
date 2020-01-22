6440e97372293186e759f9da915901f876f9c2a2
Maryia <mashaduk1996@gmail.com>
2020-01-22 17:02:10 +0300

    fix 547456456

--- app/src/main/java/com/gmail/mashaduk1996/weather/SettingsActivity.java
+++ app/src/main/java/com/gmail/mashaduk1996/weather/SettingsActivity.java
@@ -1,8 +1,10 @@
 package com.gmail.mashaduk1996.weather;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 
@@ -15,10 +17,13 @@ public class SettingsActivity extends PreferenceActivity {
 
     }
 
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         Intent intent = new Intent(SettingsActivity.this, StartActivity.class);
         startActivity(intent);
     }
+
+
 }
